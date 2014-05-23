ef7687ec209949cfb9181c7a72c6b85d8d39b2c9
imullins91 <patmullins3@gmail.com>
2014-05-22 21:44:17 -0700

    compile errors

--- src/main/java/com/patmullins/soccerleague/Player.java
+++ src/main/java/com/patmullins/soccerleague/Player.java
@@ -7,17 +7,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class Player extends HttpServlet
-{
+public class Player extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    {
+        {
 
-        String name = request.getParameter("name");
-        String position = request.getParameter("position");
-        String jersey = request.getParameter("jersey");
+            String name = request.getParameter("name");
+            String position = request.getParameter("position");
+            String jersey = request.getParameter("jersey");
 
-        request.getRequestDispatcher("Player");
-        rd.forward(request, response);
-
+            request.getRequestDispatcher("Player").forward(request, response);
+        }
     }
 }
