21dd009023c81f8a6e8db1c6d67f820a6d226359
imullins91 <patmullins3@gmail.com>
2014-06-03 17:08:30 -0700

    Linked the jsp and servlet

--- src/main/java/com/patmullins/soccerleague/PlayerServlet.java
+++ src/main/java/com/patmullins/soccerleague/PlayerServlet.java
@@ -1,22 +1,22 @@
 package com.patmullins.soccerleague;
 
-import javax.servlet.*;
-import java.io.IOException;
-import java.io.PrintWriter;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-public class PlayerServlet extends HttpServlet{
 
-        public void doGet(HttpServletRequest request,
-                          HttpServletResponse response)
-                throws ServletException, IOException {
-        }
-        public void doPost(HttpServletRequest request,
-                       HttpServletResponse response)
-            throws ServletException, IOException {
-        doGet(request, response);
+        import javax.servlet.ServletException;
+        import javax.servlet.http.HttpServlet;
+        import javax.servlet.http.HttpServletRequest;
+        import javax.servlet.http.HttpServletResponse;
+        import java.io.IOException;
 
+public class PlayerServlet extends HttpServlet {
 
-            }
-        }
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("GET Pat Mullins");
+        response.getWriter().println("GET");
+        response.getWriter().close();
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("POST Pat Mullins");
+    }
+
+}
