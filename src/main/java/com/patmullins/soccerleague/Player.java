be2c5d79c08154cefb94842c673bd13bdca3571a
Dan Mullins <dmullins78@gmail.com>
2014-06-23 17:24:57 -0500

    more commits

---
+++ src/main/java/com/patmullins/soccerleague/Player.java
@@ -0,0 +1,21 @@
+package com.patmullins.soccerleague;
+
+public class Player {
+    String name;
+
+    public String getName() {
+
+        return name;
+    }
+ public void setName(String name){
+
+     this.name= name;
+
+
+ }
+
+
+
+
+    }
+
--- src/main/java/com/patmullins/soccerleague/PlayerServlet.java
+++ src/main/java/com/patmullins/soccerleague/PlayerServlet.java
@@ -7,17 +7,20 @@ import java.io.IOException;
 
 public class PlayerServlet extends HttpServlet {
 
-    public static void main(String [] args) {
-        System.out.println("Here is the information you entered");
-    }
     public void doGet(HttpServletRequest request, HttpServletResponse response)throws IOException, ServletException {
+        Player messi = new Player();
+        messi.setName("Messi");
+        RequestDispatcher rd = request.getRequestDispatcher("PlayerInfo.jsp");
+        rd.forward(request, response);
+        String name = "firstName";
+        messi.setName(request.getParameter(name));
 
-        PrintWriter out = response.getWriter();
-        out.println("<h1>DSM Soccer League Information: </h1>");
-        out.println("<b><BR>Player has entered: </b>");
-        out.println("<BR>First Name: " + request.getParameter("firstName"));
-        out.println("<BR>Last Name: " + request.getParameter("lastName"));
-        out.println("<BR>Preferred Position: " + request.getParameter("position"));
-        out.println("<BR>Jersey No: " + request.getParameter("jersey"));
+//        PrintWriter out = response.getWriter();
+//        out.println("<h1>DSM Soccer League Information: </h1>");
+//        out.println("<b><BR>Player has entered: </b>");
+//        out.println("<BR>First Name: " + request.getParameter("firstName"));
+//        out.println("<BR>Last Name: " + request.getParameter("lastName"));
+//        out.println("<BR>Preferred Position: " + request.getParameter("position"));
+//        out.println("<BR>Jersey No: " + request.getParameter("jersey"));
     }
 }
