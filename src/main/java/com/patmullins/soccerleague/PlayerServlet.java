6cbb9b8e80b34d5067baa30c92b297fb82ae591d
imullins91 <patmullins3@gmail.com>
2014-06-25 21:26:22 -0700

    Communicated with the jsp and servlet
    Still cant get any data to appear on the page i have tried every
    combination and no prevail.

--- src/main/java/com/patmullins/soccerleague/PlayerServlet.java
+++ src/main/java/com/patmullins/soccerleague/PlayerServlet.java
@@ -15,21 +15,15 @@ public class PlayerServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        //communicating a simple String message.
-
         String name = "Example source code of Servlet to JSP communication.";
 
         request.setAttribute("messi", name);
-
-        //communicating a Vector object
 
         Player messi = new Player();
 
         messi.getName();
 
         request.setAttribute("messi", name);
-
-        //Servlet JSP communication
 
         RequestDispatcher reqDispatcher = getServletConfig().getServletContext().getRequestDispatcher("/index.jsp");
 
