f46d64be0d83f3ca96a09eea1fddc66bd000bf3c
imullins91 <patmullins3@gmail.com>
2014-08-12 15:16:54 -0500

    Update Team.java

--- src/main/java/com/patmullins/soccerleague/domain/Team.java
+++ src/main/java/com/patmullins/soccerleague/domain/Team.java
@@ -1,4 +1,33 @@
 package com.patmullins.soccerleague.domain;
 
 public class Team {
+
+        private String teamName;
+        private String competitionLevel;
+        private String teamCity;
+
+        public String getTeamName() {
+            return teamName;
+        }
+
+        public void setTeamName(String teamName) {
+            this.teamName = teamName;
+        }
+
+        public String getTeamCity() {
+            return teamCity;
+        }
+
+        public void setTeamCountry(String teamCity) {
+            this.teamCity = teamCity;
+        }
+
+        public String getCompetitionLevel() {
+            return competitionLevel;
+        }
+
+        public void setCompetitionLevel(String competitionLevel) {
+            this.competitionLevel = competitionLevel;
+        }
+
 }
