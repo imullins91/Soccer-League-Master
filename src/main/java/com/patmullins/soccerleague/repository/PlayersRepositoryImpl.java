34bdde5df14f7bcd10514c0d1d68931cfe25d1d8
pmullins91 <patmullins3@gmail.com>
2014-08-14 15:41:12 -0500

    Update PlayersRepositoryImpl.java

--- src/main/java/com/patmullins/soccerleague/repository/PlayersRepositoryImpl.java
+++ src/main/java/com/patmullins/soccerleague/repository/PlayersRepositoryImpl.java
@@ -5,6 +5,7 @@ import com.patmullins.soccerleague.repository.jdbc.JDBCTemplate;
 import com.patmullins.soccerleague.repository.jdbc.SQLQueryWrapper;
 import com.patmullins.soccerleague.repository.jdbc.SQLStatementWrapper;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -24,21 +25,33 @@ public class PlayersRepositoryImpl implements PlayersRepository {
             public void execute(Statement statement) throws SQLException {
 
 
-                statement.execute("insert into Players (firstName, lastName, playerPosition , country, jersey) values ('"+ player.getFirstName() + "', '"+ player.getLastName() + "','"+ player.getPosition() + "','"+ player.getCountry() + "', '"+ player.getJersey() + "')");
+                statement.execute("insert into Players (firstName, lastName, playerPosition , country, jersey) values ('" + player.getFirstName() + "', '" + player.getLastName() + "','" + player.getPosition() + "','" + player.getCountry() + "', '" + player.getJersey() + "')");
             }
         });
     }
 
     public List<Player> findAllPlayers() {
-        final ArrayList<Player> Players = new ArrayList<Player>();
-        jdbcTemplate.query("select * from 'Players'", new SQLQueryWrapper() {
+        final ArrayList<Player> players = new ArrayList<Player>();
+        jdbcTemplate.query("select * from players", new SQLQueryWrapper() {
             public void interpretResults(ResultSet resultSet) throws SQLException {
-                Player player = new Player();
+                while (resultSet.next()) {
+                    String firstName = resultSet.getString("firstName");
+                    String lastName = resultSet.getString("firstName");
+                    String playerPosition = resultSet.getString("firstName");
+                    String country = resultSet.getString("firstName");
+                    int jersey= resultSet.getInt("firstName");
+                    Player player = new Player();
+                    player.setFirstName(resultSet.getString("firstName"));
+                    player.setLastName(resultSet.getString("lastName"));
+                    player.setPosition(resultSet.getString("playerPosition"));
+                    player.setCountry(resultSet.getString("country"));
+                    player.setJersey(resultSet.getInt("jersey"));
+                    players.add(player);
 
-                Players.add(player);
+                }
             }
         });
 
-        return Players;
+        return players;
     }
 }
