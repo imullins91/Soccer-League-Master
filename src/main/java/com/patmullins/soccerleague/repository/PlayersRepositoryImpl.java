ddc99a04dce1e5a5039973ee04d75f954affe8aa
pmullins91 <patmullins3@gmail.com>
2014-08-19 09:28:21 -0500

    Update PlayersRepositoryImpl.java

--- src/main/java/com/patmullins/soccerleague/repository/PlayersRepositoryImpl.java
+++ src/main/java/com/patmullins/soccerleague/repository/PlayersRepositoryImpl.java
@@ -32,7 +32,7 @@ public class PlayersRepositoryImpl implements PlayersRepository {
 
     public List<Player> findAllPlayers() {
         final ArrayList<Player> players = new ArrayList<Player>();
-        jdbcTemplate.query("select * from 'Players'", new SQLQueryWrapper() {
+        jdbcTemplate.query("select * from Players", new SQLQueryWrapper() {
             public void interpretResults(ResultSet resultSet) throws SQLException {
                 while (resultSet.next()) {
                     Player player = new Player();
